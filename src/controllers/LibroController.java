7b925fea8be18b73e744132388a4282b0523e5c6
Leandrito21 <leanrame1@gmail.com>
2025-07-16 16:49:35 -0500

    otro comit

--- src/controllers/LibroController.java
+++ src/controllers/LibroController.java
@@ -6,9 +6,7 @@ import models.Book;
 public class LibroController {
 
     public Set<Book> procesarLibros(List<Book> libros) {
-        Comparator<Book> comparator = Comparator
-                .comparing(Book::getTitulo, Comparator.reverseOrder())
-                .thenComparingInt(Book::getAnio);
+        Comparator<Book> comparator = Comparator.comparing(Book::getTitulo, Comparator.reverseOrder()).thenComparingInt(Book::getAnio);
 
         Set<Book> ordenados = new TreeSet<>(comparator);
         ordenados.addAll(libros);
